8e5c2b11df49e88424faed0a6a11eaaa582cb4e8
mary <marialauraschosslerpereira@gmail.com>
2025-03-11 19:24:43 -0300

    pag e desing novos

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ form?.addEventListener('submit', (e) => {
     e.preventDefault();
     alert('Filtro aplicado! 🌸 Exibindo plantas compatíveis.');
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const card = document.querySelector(".card");
+
+    // Animação ao carregar
+    setTimeout(() => {
+        card.style.opacity = "1";
+        card.style.transform = "translateY(0)";
+    }, 200);
+});
